b276750bbd5fee9ea8ef5628275bd021aeac2360
AdairLi2504 <AdairLi0309@outlook.com>
2026-07-12 17:30:58 +0800

    playground(vision&audio)

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -4,10 +4,11 @@ import { checkPromptAvailability, downloadModel, createChatProvider } from "xsai
 import { ref, onMounted, toRaw, computed } from "vue";
 import { generateText } from "@xsai/generate-text";
 import { streamText } from "@xsai/stream-text";
-import type { Message } from '@xsai/shared-chat'
+import type { Message, CommonContentPart } from '@xsai/shared-chat'
 
 import { useDark, useToggle } from '@vueuse/core'
 import Progress from "./components/Progress.vue";
+import InputFile from "./components/InputFile.vue"
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
@@ -27,22 +28,55 @@ async function download() {
   availability.value = resultAvailability
 }
 
+
+
 // conversation
 const systemPromptInput = ref('')
 const userInput = ref('')
 const isExecuting = ref(false)
+const filesInput = ref<File[]>([])
+
+// input files
+
+function handleFilesChange(files: File[]) {
+  filesInput.value = [...filesInput.value, ...files]
+}
+
+function deleteFile(file: File) {
+  filesInput.value = filesInput.value.filter(i => i != file)
+}
+
 // not include System Prompt
 const contextMessages = ref<Message[]>([])
 const isStream = ref(false)
 const streamOutput = ref('')
+const chatProvider = createChatProvider({ expectedInputs: [{ type: 'audio' }, { type: 'image' }] })
 
 async function execute() {
-  const chatProvider = createChatProvider()
+  function fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        // reader.result 是 Data URL，形如 "data:image/png;base64,xxxx"
+        const dataUrl = reader.result as string;
+        resolve(dataUrl);
+      };
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
+  }
+  function isWav(type: string) {
+    const AUDIO_WAV = new Set<string>(['audio/wav','audio/x-wav','audio/vnd.wave']);
+    return AUDIO_WAV.has(type)
+  }
   isExecuting.value = true
   streamOutput.value = ''
   // backup user input
   let tempUserInput = userInput.value
   userInput.value = ""
+  let tempFileInput = filesInput.value
+  console.log(tempFileInput);
+  filesInput.value = []
   // Convert Vue reactive Messages to plain objects for structuredClone.
   let pureMessages = structuredClone(toRaw(contextMessages.value)) as Message[]
   // Construct System Prompt Message
@@ -53,16 +87,64 @@ async function execute() {
     }
     pureMessages = [systemPromptMessage, ...pureMessages]
   }
+  let contentInput: CommonContentPart[] | string
+  if (tempFileInput.length > 0) {
+    //convert audio and image file
+    let contentParts: CommonContentPart[] = []
+    for (const f of tempFileInput) {
+      // mp3
+      if (f.type === "audio/mpeg") {
+        const base64 = await fileToBase64(f)
+        contentParts.push({
+          type: 'input_audio',
+          input_audio: {
+            format: 'mp3',
+            data: base64.split(',')[1] as string
+          }
+        })
+        // wav
+      }else if(isWav(f.type)){
+        const base64 = await fileToBase64(f)
+        contentParts.push({
+          type: 'input_audio',
+          input_audio: {
+            format: 'wav',
+            data: base64.split(',')[1] as string
+          }
+        })
+      // image
+      }else if(f.type.startsWith("image/")){
+        const base64 = await fileToBase64(f)
+        contentParts.push({
+          type:'image_url',
+          image_url:{
+            url:base64
+          }
+        })
+      }else{
+        userInput.value = tempUserInput
+        filesInput.value = tempFileInput
+        isExecuting.value = false
+        alert("Unsupported File! Only mp3/wav Audio and Image Are Acceptable")
+        return new Error("UnsupportedFile")
+      }
+    }
+    contentParts.push({
+        type:'text',
+        text:tempUserInput
+    })
+    contentInput = contentParts
+  } else contentInput = tempUserInput
   const userMessage: Message = {
     'role': 'user',
-    'content': tempUserInput,
+    'content': contentInput,
   }
   pureMessages = [...pureMessages, userMessage]
   contextMessages.value = pureMessages
   try {
     if (isStream.value) {
-      const { textStream,messages } = streamText({...chatProvider.chat(),messages: pureMessages})
-      for await (const textPart of textStream){
+      const { textStream, messages } = streamText({ ...chatProvider.chat(), messages: pureMessages })
+      for await (const textPart of textStream) {
         streamOutput.value = streamOutput.value + textPart
       }
       contextMessages.value = await messages
@@ -72,6 +154,7 @@ async function execute() {
     }
   } catch {
     userInput.value = tempUserInput
+    filesInput.value = tempFileInput
   } finally {
     isExecuting.value = false
   }
@@ -136,6 +219,13 @@ const contextMessagesWithoutSystemPrompt = computed(() =>
       <div>
         <textarea v-model="userInput" h-full w-full rounded-lg bg="neutral-100 dark:neutral-800" p-4 font-mono />
       </div>
+      <ol flex flex-row gap-2 flex-wrap>
+        <li border-2 rounded-xl py-2 px-4 flex flex-row border-neutral-200 dark:border-neutral-700 gap-1 items-center
+          v-for="f in filesInput">
+          <button i-at-icons:cross @click="deleteFile(f)" />
+          <span>{{ f.name }}</span>
+        </li>
+      </ol>
       <div flex flex-row gap-2>
         <button rounded-lg bg="blue-100 dark:blue-900" px-4 py-2 flex items-center gap-2 @click="execute"
           :disabled="isExecuting || userInput === '' || availability != 'available'">
@@ -151,6 +241,7 @@ const contextMessagesWithoutSystemPrompt = computed(() =>
           :disabled="isExecuting">
           Clean
         </button>
+        <InputFile px-4 py-2 @update:model-value="handleFilesChange" />
         <div px-4 py-2 flex items-center gap-2>
           <input type="checkbox" v-model="isStream" />
           Stream
